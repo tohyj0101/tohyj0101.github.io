27ed5afd5126b51f298654a639c0ab27d2971d52
Toh Yong Jie <83193430+tohyj0101@users.noreply.github.com>
2023-07-13 18:12:26 +0800

    Update journey.js

--- js/journey.js
+++ js/journey.js
@@ -2,42 +2,42 @@
 const journey_tab = document.querySelectorAll(".journey-tab");
 
 journey_tab.forEach((tab) => {
-  try {
-    const arrow = tab.querySelector("i");
-    const container = tab.querySelector(".sub-text");
+	try {
+		const arrow = tab.querySelector("i");
+		const container = tab.querySelector(".sub-text");
 
-    arrow.addEventListener('click',function(event) {
-        event.preventDefault();
+		arrow.addEventListener('click', function(event) {
+			event.preventDefault();
 
-        if (!container.classList.contains('active')) {
-            
-            arrow.setAttribute("data-cur","180");
-            
-            container.classList.add('active');
-            container.style.height = 'auto';
-      
-            var height = container.clientHeight + 'px';
-      
-            container.style.height = '0px';
-      
-            setTimeout(function () {
-              container.style.height = height;
-            }, 0);
-            
-          } else {
+			if (!container.classList.contains('active')) {
 
-            arrow.setAttribute("data-cur","0");
-            
-            container.style.height = '0px';
-            container.addEventListener('transitionend', function () {
-              container.classList.remove('active');
-            }, {
-              once: true
-            })
-        }
-    })
-  } catch (error) {
-    
-  }
-    
+				arrow.setAttribute("data-cur", "180");
+
+				container.classList.add('active');
+				container.style.height = 'auto';
+
+				var height = container.clientHeight + 'px';
+
+				container.style.height = '0px';
+
+				setTimeout(function() {
+					container.style.height = height;
+				}, 0);
+
+			} else {
+
+				arrow.setAttribute("data-cur", "0");
+
+				container.style.height = '0px';
+				container.addEventListener('transitionend', function() {
+					container.classList.remove('active');
+				}, {
+					once: true
+				})
+			}
+		})
+	} catch (error) {
+
+	}
+
 })
