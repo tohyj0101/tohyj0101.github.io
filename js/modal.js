1966f664412454193b2b78e6abd6068163339791
Toh Yong Jie <83193430+tohyj0101@users.noreply.github.com>
2023-07-13 18:14:12 +0800

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -9,30 +9,30 @@ var span = document.querySelectorAll("#services-modal .close");
 
 // When the user clicks on the button, open the modal
 btn.forEach((button, index) => {
-    button.onclick = function() {
-        modal.classList.toggle("active");
-        modal.style.display = "grid";
-        container[index].classList.toggle("active");
-        console.log(index)
-    }
+	button.onclick = function() {
+		modal.classList.toggle("active");
+		modal.style.display = "grid";
+		container[index].classList.toggle("active");
+		console.log(index)
+	}
 })
 
 // When the user clicks on <span> (x), close the modal
 span.forEach((close, index) => {
-    close.onclick = function() {
-        modal.style.display = "none";
-        modal.classList.toggle("active");
-        container[index].classList.toggle('active')
-    }
+	close.onclick = function() {
+		modal.style.display = "none";
+		modal.classList.toggle("active");
+		container[index].classList.toggle('active')
+	}
 })
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.classList.toggle("active");
-    modal.style.display = "none";
-    container.forEach((block) => {
-        block.classList.remove('active');
-    })
-  }
+	if (event.target == modal) {
+		modal.classList.toggle("active");
+		modal.style.display = "none";
+		container.forEach((block) => {
+			block.classList.remove('active');
+		})
+	}
 }
